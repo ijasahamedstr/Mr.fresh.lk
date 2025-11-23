5b67f827582a35e349fef0554cc46f306f76659c
ijasahamedstr <ijasahamed.str@gmail.com>
2025-11-23 17:58:59 +0300

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:5173",
       "https://digilasersa-admin.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
