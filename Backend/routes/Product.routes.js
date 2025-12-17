f6ac77496a43b48b29a4daeb0243d10287c52a18
ijasahamedstr <ijasahamed.str@gmail.com>
2025-12-17 11:59:51 +0300

    commit

--- Backend/routes/Product.routes.js
+++ Backend/routes/Product.routes.js
@@ -4,21 +4,10 @@ import { createProduct, getProducts } from '../controller/Product.controller.js'
 // Create a new router instance
 const Productssection = express.Router();
 
-
 // Create the Data Register
 Productssection.post('/',createProduct);
 
 // View the Data Register
 Productssection.get('/',getProducts);
-
-// View the Single Data Register
-Productssection.get("/:id",);
-
-//Delete Data Register
-Productssection.delete('/:id',);
-
-//Update Data Register
-Productssection.put('/:id',);
-
 
 export default Productssection;
