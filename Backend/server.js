95b3ab35fc360a37e9977e8f663779e2cee499dc
ijasahamedstr <ijasahamed.str@gmail.com>
2025-12-17 12:05:11 +0300

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import AccountAdminrouter from "./routes/AccountRegisterAdmin.route.js";
 import Slidersection from "./routes/Slidersection.route.js";
 import Categorysection from "./routes/Categories.route.js";
 import Productssection from "./routes/Product.routes.js";
-import Inquirysection from "./routes/Inquiry.route.js";
+import inquiryRoutes from "./routes/Inquiry.route.js";
 
 // Create an instance of Express
 const app = express();
@@ -44,7 +44,7 @@ app.use('/Adminlogin', AccountAdminloginrouter);
 app.use('/Adminregister',AccountAdminrouter);
 app.use('/Slidersection',Slidersection);
 app.use("/Categorysection", Categorysection);
-app.use('/inquiry',Inquirysection);
+app.use('/inquiry',inquiryRoutes);
 app.use("/Products",Productssection);
 
 
