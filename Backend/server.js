06aae861b304819aa2159fec8a34e4780f640803
ijasahamedstr <116961653+ijasahamedstr@users.noreply.github.com>
2026-08-18 01:31:28 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "https://mr-fresh-lk-admin.vercel.app",
+      "https://mr-fresh-lk.vercel.app",
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
